534bb155b6bd080a9d6449b7faded77859452b18
albogachiev <albogachiev-2014@mail.ru>
2024-08-13 13:52:01 +0300

    added components: checkbox checkbox-filters-group filter-checkbox filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Categories, Container, SortPopup} from "@/components/shared";
+import {Categories, Container, Filters, SortPopup} from "@/components/shared";
 import {Title} from "@/components/shared/title";
 import TopBar from "@/components/shared/top-bar";
 
@@ -8,6 +8,22 @@ export default function Home() {
             <Title text={'Все пиццы'} size={'lg'} className={'font-extrabold'}/>
         </Container>
         <TopBar/>
+        <Container className={'mt-10 pb-14'}>
+            <div className={'flex gap-[60px]'}>
+                {/* Фильтрация */}
+                <div className={'w-[250px]'}>
+                    <Filters/>
+                </div>
+                {/*  Список товаров  */}
+                <div className="flex-1">
+                    <div className="flex flex-col gap-16">
+                       Список товаров
+                        {/*<ProductsGroupList title="Пиццы" items={[1, 2, 3, 4, 5]}/>*/}
+                        {/*<ProductsGroupList title="Комбо" items={[1, 2, 3, 4, 5]}/>*/}
+                    </div>
+                </div>
+            </div>
+        </Container>
 
     </>
 }
